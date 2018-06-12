22d50b447bb5e06eed998e61eab1b76dd8f7a3bf
Travis Allen <travisallen6@gmail.com>
2018-06-12 09:34:23 -0600

    Initial commit

---
+++ after-hours-react-calendar/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+
+    let monthDays = {
+      Jan: 31,
+      Feb: 28,
+      Mar: 31,
+      Apr: 30,
+      May: 31,
+      Jun: 30,
+      Jul: 31,
+      Aug: 31,
+      Sep: 30,
+      Oct: 31,
+      Nov: 30,
+      Dec: 31
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
